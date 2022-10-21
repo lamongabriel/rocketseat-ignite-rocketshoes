ca0c9a1a7293ef35791fd959d43d035a0b5a70f3
Gabriel Lamon <gabriel-lamon@outlook.com>
2022-10-21 16:50:01 -0300

    rocketshoes first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# RockeShoes
+
+![GitHub repo size](https://img.shields.io/github/repo-size/lamongabriel/rocketseat-ignite-rocketshoes?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/lamongabriel/rocketseat-ignite-rocketshoes?style=for-the-badge)
+![GitHub forks](https://img.shields.io/github/forks/lamongabriel/rocketseat-ignite-rocketshoes?style=for-the-badge)
+
+<img src="./rocketShoes.png" alt="rocketShoes">
+
+> Project developed in the Rocketseat ignite course to challenge the creation of a custom hook.
+
+RocketShoes is a project created carry the task of a custom hook, in this project we've developed a custom ReactJS hook called useCart that has a fully functional and operational shopping cart behind, with some features like: Adding items to cart, removing items, changing items quantity, calculating real time expenses, listing the quantity of produts and inventory management.
+
+### Fixed and upgrades
+
+This project is fully developed and properly working but it could have some changes:
+
+- [x] Inventory check
+- [x] Cart saves to localStorage
+- [x] Add, update and remove items from cart
+- [x] Real time calculation
+- [x] Custom ReactJS Hook
+- [ ] Update by more than one on cart
+- [ ] Discount area
+
+## ✨ Live Project
+
+The project is live and working! You can check it out here:
+
+[https://lamongabriel-rocketshoes.netlify.app/](https://lamongabriel-rocketshoes.netlify.app/)
+
+## 💻 Prerequisites
+
+Before going on, make sure you have the most recent versions of the following packages:
+
+- NodeJS
+- NPM
+
+## 🚀 Installing <ROCKETSHOES>
+
+To use it just run:
+
+Linux, macOS and Windows:
+
+```
+git clone https://github.com/lamongabriel/rocketseat-ignite-rocketshoes.git
+cd /rocketseat-ignite-rocketshoes/
+yarn
+yarn start
+```
+
+You also need to change the api route to your localhost port. In order to do that, go to `/services/api.ts`. Then change `/baseURL: 'https://lamongabriel-rocketshoes.netlify.app/'` to your local adress, the default one is `http://localhost:3000/api`.
+
+## ☕ Usage <DT_MONEY>
+
+Check out the custom hook in the folder hooks, or use the website to see it performing. Adding products, removing them and updating
+
+## 💡 Technologies
+
+This project was developed using severous technologies to help organize, maintain and develop it.
+
+- ReactJS
+- TypeScript
+- Styled Components
+- Polished
+- Axios
+- JSON Server
+- HTML, CSS and JS
---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,134 @@
+import { createContext, ReactNode, useContext, useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { api } from '../services/api';
+import { Product } from '../types';
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+interface UpdateProductAmount {
+  productId: number;
+  amount: number;
+}
+
+interface CartContextData {
+  cart: Product[];
+  addProduct: (productId: number) => Promise<void>;
+  removeProduct: (productId: number) => void;
+  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
+}
+
+const CartContext = createContext<CartContextData>({} as CartContextData);
+
+export function CartProvider({ children }: CartProviderProps): JSX.Element {
+  const [cart, setCart] = useState<Product[]>(() => {
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart))
+  }, [cart])
+
+  const addProduct = async (productId: number) => {
+    try {
+      const temporaryCart = [...cart]
+      const productExists = temporaryCart.find(product => product.id === productId)
+      const apiResponseStock = await api.get(`/stock/${productId}`)
+
+      const warehouseStock = apiResponseStock.data.amount
+      const currentAmount = productExists ? productExists.amount : 0
+
+      if(currentAmount + 1 > warehouseStock){
+        toast.error('Selected quantity out of stock');
+        return
+      }
+
+      if(productExists){
+        productExists.amount++
+      } else{
+        const productApiResponse = await api.get(`/products/${productId}`)
+        const newProduct = {
+          ...productApiResponse.data, 
+          amount: 1
+        }
+        temporaryCart.push(newProduct)
+      }
+
+      setCart(temporaryCart);
+      toast.success('Product added to cart');
+    } catch {
+      toast.error('Error updating quantity');
+    }
+  };
+
+  const removeProduct = (productId: number) => {
+    try {
+      const temporaryCart = [...cart]
+      const indexToDelete = temporaryCart.findIndex(product => product.id === productId)
+
+      if(indexToDelete !== -1){
+        temporaryCart.splice(indexToDelete, 1)
+        setCart(temporaryCart)
+      } else{
+        throw new Error()
+      }
+
+    } catch {
+      toast.error('Error deleting product');
+    }
+  };
+
+  const updateProductAmount = async ({
+    productId,
+    amount,
+  }: UpdateProductAmount) => {
+    try {
+      if(amount <= 0){
+        return
+      }
+      
+      const apiResponseStock = await api.get(`/stock/${productId}`)
+      const warehouseStock = apiResponseStock.data.amount
+
+      if(amount > warehouseStock){
+        toast.error('Selected quantity out of stock');
+        return
+      }
+
+      const temporaryCart = [...cart]
+      const productExists = temporaryCart.find(product => product.id === productId)
+
+      if(productExists){
+        productExists.amount = amount
+        setCart(temporaryCart)
+        toast.success('Cart updated');
+      } else{
+        throw new Error()
+      }
+
+    } catch {
+      toast.error('Error updating product quantity');
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addProduct, removeProduct, updateProductAmount }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart(): CartContextData {
+  const context = useContext(CartContext);
+
+  return context;
+}
